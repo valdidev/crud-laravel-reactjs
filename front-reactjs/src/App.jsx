d9f3ccea3e9b8e2767cc6bb7086b5b476c4dc3b0
valdidev <valdicode.fernando@gmail.com>
2022-12-10 16:56:28 +0100

    deleted: app css

--- front-reactjs/src/App.jsx
+++ front-reactjs/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShowProducts } from './components/ShowProducts';
 import { CreateProduct } from './components/CreateProduct';
